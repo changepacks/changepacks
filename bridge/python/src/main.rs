0b144ea107494b7960216a96afdd38308efb83ba
owjs3901 <owjs3901@gmail.com>
2025-10-21 23:42:25 +0900

    Implement python

--- bridge/python/src/main.rs
+++ bridge/python/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::*;
+
+#[pyo3_async_runtimes::tokio::main]
+async fn main() -> PyResult<()> {
+    cli::main(&std::env::args().skip(1).collect::<Vec<String>>())
+        .await
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string()))
 }
