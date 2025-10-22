8496d3cb0f875050573e203060522ddd994bd43e
owjs3901 <owjs3901@gmail.com>
2025-10-22 10:42:30 +0900

    RM pyo3

--- bridge/python/src/main.rs
+++ bridge/python/src/main.rs
@@ -1,6 +1,4 @@
-#[pyo3_async_runtimes::tokio::main]
-async fn main() -> pyo3::prelude::PyResult<()> {
-    cli::main(&std::env::args().skip(1).collect::<Vec<String>>())
-        .await
-        .map_err(|e| pyo3::prelude::PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string()))
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    cli::main(&std::env::args().skip(1).collect::<Vec<String>>()).await
 }
