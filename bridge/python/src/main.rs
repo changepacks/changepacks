9908eb7cb5ed31e02a9d83080640cbf75000df09
owjs3901 <owjs3901@gmail.com>
2025-10-22 10:29:06 +0900

    Rm linker

--- bridge/python/src/main.rs
+++ bridge/python/src/main.rs
@@ -1,8 +1,6 @@
-use pyo3::prelude::*;
-
 #[pyo3_async_runtimes::tokio::main]
-async fn main() -> PyResult<()> {
+async fn main() -> pyo3::prelude::PyResult<()> {
     cli::main(&std::env::args().skip(1).collect::<Vec<String>>())
         .await
-        .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string()))
+        .map_err(|e| pyo3::prelude::PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string()))
 }
