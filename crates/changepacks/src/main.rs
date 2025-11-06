cc276239c935ba56294603d8e5070cb1866e35d2
owjs3901 <owjs3901@gmail.com>
2025-11-06 21:16:03 +0900

    Fix main

--- crates/changepacks/src/main.rs
+++ crates/changepacks/src/main.rs
@@ -1,6 +1,11 @@
-use anyhow::Result;
+use std::process;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    changepacks_cli::main(std::env::args().collect::<Vec<String>>().as_slice()).await
+async fn main() {
+    if let Err(e) =
+        changepacks_cli::main(std::env::args().collect::<Vec<String>>().as_slice()).await
+    {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    }
 }
